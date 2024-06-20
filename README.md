0904ca3d1819f9b02c9849839d38e418d58b4b40
Tiles <stefan@thetilesgame.com>
2024-06-20 21:36:20 +0100

    Change wait times and add basic readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# YC Cofounder Matching Bot
+
+This repo hosts a Selenium bot that can help you find, save and contact potential cofounders on [YC Cofounder Matching](https://www.ycombinator.com/cofounder-matching).
+
+* Automatically change your profile location and **search for cofounders in different cities**
+* **Filter potential cofounders** depending on your shared interests
+* **Save founder profiles** and **contact them** using your own template messages
+* **Use ChatGPT to filter profiles**, depending on your cofounder requirements
+* Receive **reports on your email** and see how many founder profiles were skipped, saved and contacted
+* Run it with or without Docker
--- get_cofounder.py
+++ get_cofounder.py
@@ -27,12 +27,12 @@ def add_chrome_options(chrome_options):
     """
     Adds necessary Chrome options for the browser.
     """
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless=new')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--disable-infobars')
-    # chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_argument('--enable-logging')
     chrome_options.add_argument('--v=1')
     chrome_options.add_argument('--log-level=0')
